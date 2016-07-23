b1289ad6ee9980b30eec29bde598e4029647af02
Elena <Elena>
2016-07-23 22:52:02 +0300

    Css responsiveness

--- app/components/Like.jsx
+++ app/components/Like.jsx
@@ -10,7 +10,7 @@ export default class Like extends React.Component {
         };
     }
     componentDidMount() {
-        this.onLoad();  
+        this.onLoad();  ;  
     }
     onLoad() {
         var self = this;
